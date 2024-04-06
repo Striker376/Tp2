be7efb0f682888d387b359db81112bed7b8f57b1
StrikerUW05 <hamzaabouarrouche@gmail.com>
2024-04-06 14:03:03 +0000

    first commit

---
+++ src/main/java/com/example/product/ProductApplication.java
@@ -0,0 +1,53 @@
+package com.example.product;
+
+
+import com.example.product.Repos.ProductRepos;
+import com.example.product.entites.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+
+@SpringBootApplication
+public class ProductApplication implements CommandLineRunner {
+
+    @Autowired
+    ProductRepos repos;
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class, args);
+
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+       repos.save(new Product(Long.valueOf(1),6000.0,"Tv"));
+       repos.save(new Product(Long.valueOf(2),4500.0,"Imprimante"));
+       repos.save(new Product(Long.valueOf(3),800.0,"headset"));
+       List<Product> Products =repos.findAll();
+       Products.forEach(product -> {
+           System.out.println(product);
+
+       });
+        System.out.println("--------------------------");
+        List<Product> Products2 =repos.findProductByPrixGreaterThan(1000.0);
+        Products2.forEach(product -> {
+            System.out.println(product.getId());
+            System.out.println(product.getPrix());
+            System.out.println(product.getName());
+
+
+        });
+        System.out.println("--------------------------");
+        Product startwithW = repos.search("W%");
+        System.out.println(startwithW);
+        System.out.println("--------------------------");
+        List<Product> productContainsv=repos.findProductByNameContains("v");
+        productContainsv.forEach(p->{
+            System.out.println(p.getName());
+            repos.delete(p);
+        });
+    }
+}
